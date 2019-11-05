f00a4f968f9d0b2cc184ea95397b6177b81753e6
pighui <pighui233@163.com>
2019-11-05 17:22:32 +0800

    新增UA池中间件

--- dushu/middlewares.py
+++ dushu/middlewares.py
@@ -4,8 +4,25 @@
 #
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+import random
 
 from scrapy import signals
+from scrapy.downloadermiddlewares.useragent import UserAgentMiddleware
+
+
+class RandomUserAgentMiddleware(UserAgentMiddleware):
+    def __init__(self, user_agent_list):
+        super().__init__()
+        self.user_agent_list = user_agent_list
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(user_agent_list=crawler.settings.get('USER_AGENT_LIST'))
+
+    def process_request(self, request, spider):
+        user_agent = random.choice(self.user_agent_list)
+        if user_agent:
+            request.headers['User-Agent'] = user_agent
 
 
 class DushuSpiderMiddleware(object):
