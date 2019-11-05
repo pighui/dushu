ef66fa5f31b74ea18b4ba77b5d9906e0489c3bf6
pighui <pighui233@163.com>
2019-11-05 17:19:14 +0800

    优化插入数据库的语句

--- dushu/pipelines.py
+++ dushu/pipelines.py
@@ -6,7 +6,6 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import pymysql
 from pymysql.cursors import DictCursor
-
 from dushu import settings
 
 
@@ -15,28 +14,35 @@ class DushuPipeline(object):
         # **dict 将字典转成关键参数传值的格式，如key = value
         self.conn = pymysql.Connect(**settings.DB_CONFIG)
         self.init_db()
-        self.batch_count = 0
 
     def init_db(self):
         with self.conn.cursor(cursor=DictCursor) as c:
             c.execute('drop table if exists dushu_book')
             sql = '''
             create table dushu_book(id integer PRIMARY key auto_increment,
-                                name varchar(200),
-                                author varchar(50),
-                                price FLOAT,
-                                publisher varchar(200),
-                                publish_date DATE,
-                                isbn varchar(30),
-                                detail_url varchar(100))
+                                b_name varchar(50),
+                                b_author varchar(30),
+                                b_price FLOAT,
+                                b_publisher varchar(50),
+                                b_publish_date date,
+                                b_isbn varchar(20),
+                                b_detail_url varchar(50))
             '''
             c.execute(sql)
 
     def process_item(self, item, spider):
         with self.conn.cursor(cursor=DictCursor) as c:
-            sql = 'insert into dushu_book(name,author,price,publisher,publish_date,isbn,detail_url) values(%(name)s,%(author)s,%(price)s,%(publisher)s,%(publish_date)s,%(isbn)s,%(detail_url)s)'
-            c.execute(sql,args=item)
-        self.batch_count+=1
-        if self.batch_count%100 == 0:
+            sql = "insert into dushu_book(%s) values(%s)"
+            cols = ", ".join('`{}`'.format('b_' + k) for k in item.keys())
+            val_cols = ', '.join('%({})s'.format(k) for k in item.keys())
+            res_sql = sql % (cols, val_cols)
+
+            c.execute(res_sql, args=item)
+
+
             self.conn.commit()
         return item
+
+    def close_spider(self,spider):
+        self.conn.cursor().close()
+        self.conn.close()
